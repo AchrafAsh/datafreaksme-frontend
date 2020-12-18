b062de3a4a74131fcdaad858b1d86bd3f63d70f4
AchrafAsh <achraf.ait-sidi-hammou@ensta-paris.fr>
2020-12-19 00:24:22 +0100

    Initial setup

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+
+export default function Home() {
+    return (
+        <>
+            <Head>
+                <title>DataFreaks</title>
+            </Head>
+            <main className='bg-black p-6'></main>
+        </>
+    )
+}
